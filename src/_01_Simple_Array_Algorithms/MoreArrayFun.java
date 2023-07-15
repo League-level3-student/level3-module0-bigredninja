838931c752de739388c90b13e2b01c41d51e2d8b
gc1-2 <gc1-2@LAPTOP-R9ASR0AR>
2023-07-15 11:26:55 -0700

    hehehehee

--- src/_01_Simple_Array_Algorithms/MoreArrayFun.java
+++ src/_01_Simple_Array_Algorithms/MoreArrayFun.java
@@ -1,26 +1,53 @@
 package _01_Simple_Array_Algorithms;
 
 import java.util.Random;
+import java.util.concurrent.ForkJoinWorkerThread;
 
 public class MoreArrayFun {
-    //1. Create a main method to test the other methods you write.
+	// 1. Create a main method to test the other methods you write.
+	public static void main(String[] args) {
+		MoreArrayFun arrayfun = new MoreArrayFun();
+		String[] monkey = { "one", "two", "three", "four" };
+		Random rnd = new Random();
+	}
 
+	// 2. Write a method that takes an array of Strings and prints all the Strings
+	// in the array.
+	void stringPrint(String[] array) {
+		for (int i = 0; i < array.length; i++) {
+			String monkey1 = array[i];
+			System.out.println(monkey1);
+		}
+	}
 
+	// 3. Write a method that takes an array of Strings and prints all the Strings
+	// in the array
+	// in reverse order.
+	void stringReverse(String[] array) {
+		for (int i = array.length-1; i >= 0; i--) {
+			String monkey1 = array[i];
+			System.out.println(monkey1);
+		}
+	}
 
-    //2. Write a method that takes an array of Strings and prints all the Strings in the array.
-
-
-
-    //3. Write a method that takes an array of Strings and prints all the Strings in the array
-    //   in reverse order.
-
-
-
-    //4. Write a method that takes an array of Strings and prints every other String in the array.
-
-
-    //5. Write a method that takes an array of Strings and prints all the Strings in the array
-    //   in a completely random order. Almost every run of the program should result in a different order.
-
-
+	// 4. Write a method that takes an array of Strings and prints every other
+	// String in the array.
+	void stringEveryOther(String[] array) {
+		for (int i = 0; i < array.length; i++) {
+			if (i%2 == 0) {
+				System.out.println(array[i]);
+			}
+		}
+	}
+	// 5. Write a method that takes an array of Strings and prints all the Strings
+	// in the array
+	// in a completely random order. Almost every run of the program should result
+	// in a different order.
+	void stringRandom(String[] array) {
+		for (int i = 0; i < array.length; i++) {
+			
+			System.out.println(array[i]);
+		
+		}
+	}
 }
--- src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
+++ src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
@@ -1,46 +1,50 @@
 package _01_Simple_Array_Algorithms;
 
-import java.util.Iterator;
-
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane.IconifyAction;
-
 public class _00_1D_Array_Methods {
-    //1. Complete the method so that it returns the sum of all
-    //   of the integers in the array being passed in
-    public static int sumIntArray(int[] values) {
-    	int sum = 0;
-    	for (int i : values) {
+	// 1. Complete the method so that it returns the sum of all
+	// of the integers in the array being passed in
+	public static int sumIntArray(int[] values) {
+		int sum = 0;
+		for (int i : values) {
 			sum += i;
 		}
-        return sum;
-    }
+		return sum;
+	}
 
-    //2. Complete the method so that it returns the average of all
-    //   of the integers in the array being passed in
-    public static double averageIntArray(int[] values) {
-    	int sum = 0;
-    	for (int i : values) {
+	// 2. Complete the method so that it returns the average of all
+	// of the integers in the array being passed in
+	public static double averageIntArray(int[] values) {
+		int sum = 0;
+		double average;
+		for (int i : values) {
 			sum += i;
 		}
-        return sum/values.length;
-    }
+		average = (double) sum / values.length;
+		return average;
+	}
+	
+	// 3. Complete the method so that it returns true if the integer
+	// array contains the value specified by the second parameter.
+	// It should otherwise return false.
+	public static boolean containsIntValue(int[] array, int value) {
+		boolean contains = false;
+		for (int i : array) {
+			if (i == value) {
+				contains = true;
+			}
+		}
+		return contains;
+	}
 
-
-    //3. Complete the method so that it returns true if the integer	
-    //   array contains the value specified by the second parameter.
-    //   It should otherwise return false.
-  public static boolean containsIntValue(int[] array, int value) {
-    	boolean contains = false;
-    	for()
-        return contains;
-    }
-
-    //4. Complete the method so that it returns the index of the,
-    //   first instance that the specified value occurs in the array.
-    //   If the array does not contain the specified value, it should return -1.
-    public static int getIndex(int[] arr, int value) {
-
-        return 0;
-    }
+	// 4. Complete the method so that it returns the index of the,
+	// first instance that the specified value occurs in the array.
+	// If the array does not contain the specified value, it should return -1.
+	public static int getIndex(int[] arr, int value) {
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == value) {
+				return i;
+			}
+		}
+		return -1;
+	}
 }
-
