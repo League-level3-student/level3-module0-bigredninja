c94be02c30660bd6a74ab230def2f0e5f66b3147
gc1-2 <gc1-2@LAPTOP-R9ASR0AR>
2023-08-05 11:27:44 -0700

    finishing more array fun

--- src/_01_Simple_Array_Algorithms/MoreArrayFun.java
+++ src/_01_Simple_Array_Algorithms/MoreArrayFun.java
@@ -6,14 +6,19 @@ import java.util.concurrent.ForkJoinWorkerThread;
 public class MoreArrayFun {
 	// 1. Create a main method to test the other methods you write.
 	public static void main(String[] args) {
-		MoreArrayFun arrayfun = new MoreArrayFun();
+
 		String[] monkey = { "one", "two", "three", "four" };
-		Random rnd = new Random();
+		MoreArrayFun arrayfun = new MoreArrayFun();
+		arrayfun.stringPrint(monkey);
+		arrayfun.stringReverse(monkey);
+		arrayfun.stringEveryOther(monkey);
+		arrayfun.stringRandom(monkey);
 	}
 
 	// 2. Write a method that takes an array of Strings and prints all the Strings
 	// in the array.
 	void stringPrint(String[] array) {
+		System.out.println("*** stringPrint ***");
 		for (int i = 0; i < array.length; i++) {
 			String monkey1 = array[i];
 			System.out.println(monkey1);
@@ -24,7 +29,8 @@ public class MoreArrayFun {
 	// in the array
 	// in reverse order.
 	void stringReverse(String[] array) {
-		for (int i = array.length-1; i >= 0; i--) {
+		System.out.println("*** stringReverse ***");
+		for (int i = array.length - 1; i >= 0; i--) {
 			String monkey1 = array[i];
 			System.out.println(monkey1);
 		}
@@ -33,21 +39,25 @@ public class MoreArrayFun {
 	// 4. Write a method that takes an array of Strings and prints every other
 	// String in the array.
 	void stringEveryOther(String[] array) {
+		System.out.println("*** stringEveryOther ***");
 		for (int i = 0; i < array.length; i++) {
-			if (i%2 == 0) {
+			if (i % 2 == 0) {
 				System.out.println(array[i]);
 			}
 		}
 	}
+
 	// 5. Write a method that takes an array of Strings and prints all the Strings
 	// in the array
 	// in a completely random order. Almost every run of the program should result
 	// in a different order.
 	void stringRandom(String[] array) {
+		System.out.println("*** stringRandom ***");
+		Random rnd = new Random();
 		for (int i = 0; i < array.length; i++) {
-			
-			System.out.println(array[i]);
-		
+			int rando = rnd.nextInt(array.length);
+			System.out.println(array[rando]);
+
 		}
 	}
 }
